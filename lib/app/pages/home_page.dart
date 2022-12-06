210cc7f473c4587e88c79086b70f2710b77c793d
IDHaunter <icedhaunter@gmail.com>
2022-12-06 21:43:20 +0300

    inheritwidget

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -20,6 +20,12 @@ class HomePage extends StatelessWidget {
             height: 20,
           ),
           SomeDataS(),
+          SizedBox(
+            height: 20,
+          ),
+          DataProviderInherited(
+              value: 1000,
+              child: ShowMeAll()),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -40,5 +46,41 @@ class SomeDataS extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextField(onChanged: (newData) => context.read<DataGlobal>().changeString(newData));
+  }
+}
+
+class DataProviderInherited extends InheritedWidget {
+  final int value;
+  const DataProviderInherited({
+    Key? key,
+    required this.value,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  static DataProviderInherited of(BuildContext context) {
+    final DataProviderInherited? result =
+        context.dependOnInheritedWidgetOfExactType<DataProviderInherited>();
+    assert(result != null, 'No DataProviderInherited found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(DataProviderInherited old) {
+    //если предыдущее и текущее значения разные, то true (нужно обновлять) иначе false
+    return value != old.value;
+  }
+}
+
+class ShowMeAll extends StatelessWidget {
+  const ShowMeAll({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //получаем и подписываемся на изменения
+    //т.е. если инхерит переконструеруется и изменит свои данные, то вызовется и этот build
+    //даже если этот класс будет const что хорошо для оптимизации
+    int i = context.dependOnInheritedWidgetOfExactType<DataProviderInherited>()?.value ?? 0;
+
+    return Text('das auto $i');
   }
 }
